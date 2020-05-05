8c283c758fef6304804f1424b349c2797fe15b3e
CirillaQL <1194946223@qq.com>
2020-05-05 20:43:28 +0800

    add more IOCP FTPServer

--- Net/AsynFTPServer.hpp
+++ Net/AsynFTPServer.hpp
@@ -132,45 +132,44 @@ unsigned int WINAPI WorkerThread(LPVOID CompletionPort)//线程的执行
     LPPER_IO_OPERATION_DATA PerIoData;
     DWORD SendBytes,RecvBytes;
     DWORD Flags = 0;
-    while(TRUE)
-    {
+    while(TRUE) {
         //等待完成端口上SOCKET的完成
         //cout<<"["<<GetCurrentProcessId()<<":"<<GetCurrentThreadId()<<"]"<<":等待完成端口上SOCKET的完成"<<endl;
-        GetQueuedCompletionStatus(ComPort,&BytesTransferred, reinterpret_cast<PULONG_PTR>((LPDWORD) &PerHandleData),(LPOVERLAPPED *)&PerIoData, INFINITE);
+        GetQueuedCompletionStatus(ComPort, &BytesTransferred, reinterpret_cast<PULONG_PTR>((LPDWORD) &PerHandleData),
+                                  (LPOVERLAPPED *) &PerIoData, INFINITE);
         //检查是否有错误产生
-        if(BytesTransferred == 0)
-        {
+        if (BytesTransferred == 0) {
             //关闭SOCKET
-            cout<<"["<<GetCurrentProcessId()<<":"<<GetCurrentThreadId()<<"]"<<PerHandleData->sock<<" SOCKET关闭"<<endl;
+            cout << "[" << GetCurrentProcessId() << ":" << GetCurrentThreadId() << "]" << PerHandleData->sock
+                 << " SOCKET关闭" << endl;
             closesocket(PerHandleData->sock);
             free(PerHandleData);
             free(PerIoData);
             continue;
         }
         //为请求服务
-        cout<<"工作线程开始处理接收处理"<<endl;
-        cout<<PerHandleData->sock<<" SOCKET收到消息 : "<<PerIoData->Buff<<endl;
+        cout << "工作线程开始处理接收处理" << endl;
+        cout << PerHandleData->sock << " SOCKET收到消息 : " << PerIoData->Buff << endl;
         //回应客户端
         //ZeroMemory(PerIoData->Buff,4096);
         string option;
         for (int i = 0; i < 4096; ++i) {
-            if(PerIoData->Buff[i] != ' '){
-                option+=PerIoData->Buff[i];
+            if (PerIoData->Buff[i] != ' ') {
+                option += PerIoData->Buff[i];
             } else
                 break;
         }
         //获取服务器端目录
-        if(option == "dir"){
+        if (option == "dir") {
             WIN32_FIND_DATA fd;
-            HANDLE hff = FindFirstFile(".\\*.*",&fd);;//建立一个线程
+            HANDLE hff = FindFirstFile(".\\*.*", &fd);;//建立一个线程
             //搜索文件
             //可以通过FindFirstFile（）函数根据当前的文件存放路径查找该文件来把待操作文件的相关属性读取到WIN32_FIND_DATA结构中去
             if (hff == INVALID_HANDLE_VALUE)//发生错误
             {
                 const char *errstr = "can't list files!\n";
                 printf("list file error!\n");
-                if (send(PerHandleData->sock, errstr, strlen(errstr), 0) == SOCKET_ERROR)
-                {
+                if (send(PerHandleData->sock, errstr, strlen(errstr), 0) == SOCKET_ERROR) {
                     printf("error occurs when senging file list!\n");
                 }
                 //closesocket(datatcps);
@@ -178,14 +177,13 @@ unsigned int WINAPI WorkerThread(LPVOID CompletionPort)//线程的执行
             }
             BOOL fMoreFiles = TRUE;
             char filerecord[4096];
-            while (fMoreFiles)
-            {//发送此项文件信息
+            while (fMoreFiles) {//发送此项文件信息
                 FILETIME ft;         //文件建立时间
                 FileTimeToLocalFileTime(&fd.ftLastWriteTime, &ft);
                 SYSTEMTIME lastwtime;     //SYSTEMTIME系统时间数据结构
                 FileTimeToSystemTime(&ft, &lastwtime);
                 char *dir = const_cast<char *>(fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY ? "<DIR>" : " ");
-                sprintf(filerecord+strlen(filerecord), "%04d-%02d-%02d %02d:%02d  %5s %10d   %-20s\n",
+                sprintf(filerecord + strlen(filerecord), "%04d-%02d-%02d %02d:%02d  %5s %10d   %-20s\n",
                         lastwtime.wYear,
                         lastwtime.wMonth,
                         lastwtime.wDay,
@@ -197,12 +195,12 @@ unsigned int WINAPI WorkerThread(LPVOID CompletionPort)//线程的执行
                 fMoreFiles = FindNextFile(hff, &fd);
             }
             send(PerHandleData->sock, filerecord, strlen(filerecord), 0);
-            ZeroMemory(&filerecord,4096);
+            ZeroMemory(&filerecord, 4096);
             Flags = 0;
-            ZeroMemory((LPVOID)&(PerIoData->Overlapped),sizeof(OVERLAPPED));
+            ZeroMemory((LPVOID) &(PerIoData->Overlapped), sizeof(OVERLAPPED));
             PerIoData->DataBuff.len = 4096;
             PerIoData->DataBuff.buf = PerIoData->Buff;
-            WSARecv(PerHandleData->sock,&(PerIoData->DataBuff),1,&SendBytes,0,&(PerIoData->Overlapped),NULL);
+            WSARecv(PerHandleData->sock, &(PerIoData->DataBuff), 1, &SendBytes, 0, &(PerIoData->Overlapped), NULL);
             cout << "工作线程返回OK" << std::endl;
         }
         else if (option == "get"){
@@ -219,6 +217,10 @@ unsigned int WINAPI WorkerThread(LPVOID CompletionPort)//线程的执行
             DWORD dwNumberOfBytesRead;
             const char * filename = name.c_str();
             hFile = CreateFile(filename,GENERIC_READ,FILE_SHARE_READ,NULL,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,NULL);
+            if (hFile == INVALID_HANDLE_VALUE) {
+                cout << "文件夹中没有该文件" << endl;
+                return 0;
+            }
             file_size = GetFileSize(hFile,NULL);
             cout << file_size <<endl;
             send(PerHandleData->sock,(char*)&file_size,sizeof(unsigned long long)+1,NULL);
